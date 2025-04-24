1bb901954aa0f074077cd7431231629ef390ef56
Arun Shekar <shekar.arun@gmail.com>
2025-04-24 15:14:38 +0530

    ui: Add dynamic image source for React image

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from '../src/assets/react-core-concepts.png'
+
 const reactDescriptions = [
   'Fundamental',
   'Crucial',
@@ -12,7 +14,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescription} React concepts you will need for almost any app you are
