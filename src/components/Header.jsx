009bc3adad5146914a66e62df31e312741434d4a
Arun Shekar <shekar.arun@gmail.com>
2025-04-25 11:02:41 +0530

    refactor: Move components to dedicated files

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import reactImg from '../assets/react-core-concepts.png'
+
+const reactDescriptions = [
+    'Fundamental',
+    'Crucial',
+    'Core'
+]
+function genRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+export default function Header() {
+    const reactDescription = reactDescriptions[genRandomInt(reactDescriptions.length)]
+
+    return (
+        <header>
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {reactDescription} React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    );
+}
