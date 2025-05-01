b189af08442215de10aaabf8e7bb12f2feaca1df
Arun Shekar <shekar.arun@gmail.com>
2025-05-01 20:22:14 +0530

    feat: Add dynamic text in tabContent

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import CoreConcept from './components/CoreConcept.jsx'
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  function handleSelect() {
-    console.log('Selected')
+  let tabContent = 'Please click a button';
+
+  function handleSelect(selectedButton) {
+    console.log(`${selectedButton} selected`)
+    tabContent = selectedButton
   }
 
   return (
@@ -40,12 +43,12 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic content
+          {tabContent}
         </section>
       </main>
     </div>
